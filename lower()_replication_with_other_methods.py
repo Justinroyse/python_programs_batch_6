44d8ee1c01a775293c19a6576d33825ae18ac6aa
Justin Royse L. Solomon <Justinroyselsolomon@gmail.com>
2025-03-26 10:39:38 +0000

    Added Pseudocode for Program 03

---
+++ lower()_replication_with_other_methods.py
@@ -0,0 +1,14 @@
+#initialize function for converting to lowercase 
+def lower_case(user_input):
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+
+    replacement_table = str.maketrans(upper_case, lower_case)
+
+    return user_input.translate(replacement_table)
+
+#initialize user input
+user_input = input("Enter")
+
+#print result with the function
