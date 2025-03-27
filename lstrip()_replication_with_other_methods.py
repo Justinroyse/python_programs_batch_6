616fcd312d7cb171ee3a6f41af5a5c7b8ab82dca
justinroyse <justinroyselsolomon@gmail.com>
2025-03-27 13:07:44 +0800

    Program 01 Bug: Also deletes the spaces between user input

--- lstrip()_replication_with_other_methods.py
+++ lstrip()_replication_with_other_methods.py
@@ -1,4 +1,11 @@
 #intialize user input variable
+user_input = input("Enter your fullname with 5 spaces at the beginning: ")
 
 #initialize startswith() function to detect spaces
+user_spaces = user_input.startswith(" ")
+
+if user_spaces == True:
+     replace = user_input.replace(" ", "")
+        
 #print result
+print(replace)
