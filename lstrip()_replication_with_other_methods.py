ceaf75bcc2b3b8673042ee6c6b3bd2c3b0748364
justinroyse <justinroyselsolomon@gmail.com>
2025-03-27 12:40:57 +0800

    Program 01 Bugs: not executing program

--- lstrip()_replication_with_other_methods.py
+++ lstrip()_replication_with_other_methods.py
@@ -1,2 +1,15 @@
 #for in if-else logic to remove spaces
+
 #initialize user input variable 
+user_input = input("Enter your name with 5 spaces beginning: ")
+
+for i in user_input:
+
+    if user_input[0:] == " ":
+        user_input.replace(" ", "")
+
+    elif user_input[0:]:
+        first_character = user_input.find(chr)
+        user_input.replace(" ", " ")
+
+print(user_input)
