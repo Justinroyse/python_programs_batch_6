206935e8e2f28d97d5746d6b797ea14f1e0d4d6a
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 16:15:35 +0800

    Program 05 Bug: only returns False

--- endswith()_replication_with_other_methods.py
+++ endswith()_replication_with_other_methods.py
@@ -1,7 +1,16 @@
 #initialize function for endswith() replication
 def check_suffix(user_input, suffix):
-    
+    suffix = user_input.find(suffix, -1)
+
+    if suffix == True:
+        return True
+    else:
+        return False
 
 #initialize user input variable
+user_input = input("Enter your desired text: ")
+print(user_input)
+suffix = input("Enter the suffix you wish to check for: ")
 
 #print result
+print(check_suffix(user_input, suffix))
