f0a190c92c03b863301db1c4edd008ca98190989
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 17:28:57 +0800

    Program 08: Finished Program, with appending into list and using islower() and isupper() functions

--- swapcase_replication_with_other_methods.py
+++ swapcase_replication_with_other_methods.py
@@ -1,5 +1,21 @@
 #initialize function for swapcase() replication
+def swap_case(user_input):
+    user_input_store = []
+
+    for letters in user_input:
+        if letters.islower():
+            user_input_store.append(letters.upper())
+        
+        elif letters in user_input:
+            user_input_store.append(letters.lower())
+        
+        else:
+            user_input_store.append(letters)
+    
+    return "".join(user_input_store)
 
 #initialize user input variable
+user_input = input("Enter your desired text: ")
 
 #print result
+print(swap_case(user_input))
