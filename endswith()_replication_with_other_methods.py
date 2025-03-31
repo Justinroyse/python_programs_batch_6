edeceff530189a8242bdd295059ac0b6a0e3183e
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 16:23:44 +0800

    Program 05: Finished, adjusted and added variables to complement for find() function

--- endswith()_replication_with_other_methods.py
+++ endswith()_replication_with_other_methods.py
@@ -1,11 +1,15 @@
 #initialize function for endswith() replication
 def check_suffix(user_input, suffix):
-    suffix = user_input.find(suffix, -1)
+    suffix_length = len(suffix)
 
-    if suffix == True:
-        return True
-    else:
+    if len(user_input) < suffix_length:
         return False
+    
+
+    position = user_input.find(suffix)
+
+
+    return position == len(user_input) - suffix_length
 
 #initialize user input variable
 user_input = input("Enter your desired text: ")
