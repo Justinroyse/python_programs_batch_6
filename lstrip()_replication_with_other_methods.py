83b44490442bec24dbd7a0b2dcf2aeb28a89dc11
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 15:21:55 +0800

    Program 01: Reset and changed program logic (Pseudocode)

--- lstrip()_replication_with_other_methods.py
+++ lstrip()_replication_with_other_methods.py
@@ -2,10 +2,6 @@
 user_input = input("Enter your fullname with 5 spaces at the beginning: ")
 
 #initialize startswith() function to detect spaces
-user_spaces = user_input.startswith(" ")
 
-if user_spaces == True:
-     replace = user_input.replace(" ", "")
         
 #print result
-print(replace)
