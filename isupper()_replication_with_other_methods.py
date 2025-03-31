720575eca9dae31ba042b1076636e6f0b0aaf80e
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 15:56:40 +0800

    Program 04: Initial Prototype

--- isupper()_replication_with_other_methods.py
+++ isupper()_replication_with_other_methods.py
@@ -1,5 +1,14 @@
 #initialize function to check for upper_case letters and return boolean
+def check_upper(user_input):
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    if len(user_input) in upper_case:
+        return True
+    else:
+        return False
 
 #initialize user input
+user_input = input("Enter your desired text: ")
 
 #print result
+print(check_upper(user_input))
