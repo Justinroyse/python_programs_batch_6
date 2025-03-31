9b0385d3a70ef0d5144c822847f845ba013bb352
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 22:35:07 +0800

    Proram 05: Finished Program, same logic as enswith() replication with adjustments

---
+++ batch_7/startswith()_replication_with_other_methods.py
@@ -0,0 +1,18 @@
+#initialize function for startswith() replication
+def check_prefix(user_input, prefix):
+
+    if len(prefix) > len(user_input):
+        return False
+    
+    position = user_input.find(prefix)
+
+    return position == 0
+
+#initialize user input variable
+user_input = input("Enter your desired text: ")
+print(user_input)
+check_if_prefix = input("Enter the prefix you wish to check for: ")
+
+#print result
+print(check_prefix(user_input, check_if_prefix))
+
