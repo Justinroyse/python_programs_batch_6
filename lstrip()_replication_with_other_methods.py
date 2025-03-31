dcb75bf45970c0c2428c42655ee72cb381bf50d4
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 15:28:52 +0800

    Program 01: Finished

--- lstrip()_replication_with_other_methods.py
+++ lstrip()_replication_with_other_methods.py
@@ -1,7 +1,16 @@
-#intialize user input variable
+#intialize function for lstrip replication
+#initialize a loop to find non-space character
+def remove_spaces_left(user_input):
+    
+     user_input_string = 0
+
+     while user_input_string < len(user_input) and user_input[user_input_string] == " ":
+          user_input_string += 1
+     
+     return user_input[user_input_string:]
+
+#intialize user input
 user_input = input("Enter your fullname with 5 spaces at the beginning: ")
 
-#initialize startswith() function to detect spaces
-
-        
-#print result
+#print result using function
+print(remove_spaces_left(user_input))
