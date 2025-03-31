611ec9bb542800793010491fae7502d10293096a
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 17:42:12 +0800

    Program 10: Finished Program by using split() function and capitalizing first letter of each by accessing list index

--- title()_replication_with_other_methods.py
+++ title()_replication_with_other_methods.py
@@ -1,6 +1,16 @@
 #initialize function for title() replicaton
 def to_title(user_input):
+    words = user_input.split()
+
+    corrected_words = []
+
+    for word in words: 
+        if word: 
+            corrected_word = word[0].upper() + word[1:].lower()
+            corrected_words.append(corrected_word)
     
+    return " ".join(corrected_words)
+
 
 #initialize user input variable
 user_input = input("Enter your desired text: ")
