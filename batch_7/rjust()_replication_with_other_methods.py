03e861614c362fa3be104e2a2f33dd2cf8e0dff6
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 22:48:05 +0800

    Proram 06: Finished Program, same logic as ljust() replication with adjustments

--- batch_7/rjust()_replication_with_other_methods.py
+++ batch_7/rjust()_replication_with_other_methods.py
@@ -1,5 +1,19 @@
 #initialize function for rjust() replication
+def add_left_spaces(user_input, spaces):
+    if len(user_input) >= spaces:
+        return user_input
+    
+    spaces_needed = spaces + len(user_input)
+
+    spaced_input = ' ' * spaces_needed + user_input  
+
+    return spaced_input
 
 #initialize user input variable
+user_input = input("Enter your desired text: ")
+print(user_input)
+spaces = int(input("Enter total spaces needed: "))
 
 #print result
+result = add_left_spaces(user_input, spaces)
+print(f"'{result}'")
