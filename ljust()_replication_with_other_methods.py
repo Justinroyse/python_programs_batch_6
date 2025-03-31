cd02c4c8aae9a669a85ae88eddc8ba34a74184b4
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 16:59:59 +0800

    Program 06: Finshed Program

--- ljust()_replication_with_other_methods.py
+++ ljust()_replication_with_other_methods.py
@@ -1,5 +1,20 @@
 #initialize function fo ljust() replication
+def add_right_spaces(user_input, spaces):
+    if len(user_input) >= spaces:
+        return user_input
+    
+    spaces_needed = spaces - len(user_input)
+
+    spaced_input = user_input + ' ' * spaces_needed
+
+    return spaced_input
 
 #initialize user input
+user_input = input("Enter your desired text: ")
+spaces = int(input("Enter the total spaces needed: "))
+
+result = add_right_spaces(user_input, spaces)
 
 #print result
+print(f"'{result}'")
+
