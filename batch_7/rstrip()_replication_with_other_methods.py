398e650d7c5b09a53b88cfa5cb2e494a365ec92b
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 21:40:48 +0800

    Program 01: Finished Program, with list indexing and while looop

--- batch_7/rstrip()_replication_with_other_methods.py
+++ batch_7/rstrip()_replication_with_other_methods.py
@@ -1,10 +1,16 @@
 #initialize function for rstrip() replication
 def rm_right_spaces(user_input):
 
-    user_input_string = -1
+    user_input_string = len(user_input) - 1
 
-    
+    while user_input_string >= 0 and user_input[user_input_string] == " ":
+        user_input_string -= 1 
+     
+    return user_input[:user_input_string + 1]
 
 #initialize user input
+user_input = input("Enter your desired text: ")
 
 #print result
+result = rm_right_spaces(user_input)
+print(f"'{result}'")
