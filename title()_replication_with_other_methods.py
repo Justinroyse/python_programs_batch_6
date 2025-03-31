6263a19d2778fca84098d622f4c4b7633fb2fd75
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 17:38:24 +0800

    Program 10: Initialize Pseudocodes

---
+++ title()_replication_with_other_methods.py
@@ -0,0 +1,9 @@
+#initialize function for title() replicaton
+def to_title(user_input):
+    
+
+#initialize user input variable
+user_input = input("Enter your desired text: ")
+
+#print result
+print(to_title(user_input))
