328b35b76e8dd8196757cecfdfb64b498e5b756c
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 21:51:21 +0800

    Program 02: Finished Program, using endswith() function to detect and remove suffix and accessing list index

--- batch_7/removesuffix()_replication_with_other_methods.py
+++ batch_7/removesuffix()_replication_with_other_methods.py
@@ -2,10 +2,14 @@
 #initialize a endswith() function to check for suffix and remove it
 def remove_suffix(user_input, remove):
     if user_input.endswith(remove):
-        return user_input[:len(remove)]
+        return user_input[:-len(remove)]
     else:
         return user_input
     
 #initialize user input variable
+user_input = input("Enter your desired text: ")
+print(user_input)
+remove_words = input("Enter prefix to remove: ")
 
 #print result
+print(remove_suffix(user_input, remove_words))
