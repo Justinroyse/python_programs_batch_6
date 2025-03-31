2db0a5a40c8f58898297a3519a9b048f41e9657e
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 17:35:21 +0800

    Program 09: Iniatilized Pseudocodes and Finished Program with accessing index in list program logic

---
+++ capitalize()_replication_with_other_methods.py
@@ -0,0 +1,15 @@
+#initialize function for capitalize() replication
+def capital_first(user_input):
+    if not user_input: 
+        return user_input
+    
+    first_letter = user_input[0].upper()
+    other_letters = user_input[1:].lower()
+
+    return first_letter + other_letters
+
+#initialize user input
+user_input = input("Enter your desired text: ")
+
+#print result
+print(capital_first(user_input))
