83a273fa4dc63efce5280e987c034bd337d892af
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 23:12:55 +0800

    Proram 09: Finished Program, implemented for loop and list indexing

--- batch_7/index()_replication_with_other_methods.py
+++ batch_7/index()_replication_with_other_methods.py
@@ -1,5 +1,17 @@
 #initialize function for index() replication
+def find_index(user_input, find): 
+    
+    find_length = len(find)
+
+    for i in range(len(user_input) - find_length + 1):
+        if user_input[i:i + find_length] == find:
+            return i
+    
+    raise ValueError("Not found")
 
 #initialilze user input variables
+user_input = input("Enter your desired text: ")
+find = input("Enter word you wish to find first occurence: ")
 
 #print result
+print(find_index(user_input, find))
