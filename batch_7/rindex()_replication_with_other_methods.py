9cb0b832761a9a544ddbab1ef2a2d8c609d5f38f
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 23:17:49 +0800

    Proram 10: Finished Program, same logic as index() replication, adjusted indexing to negative to start counting from last letter

--- batch_7/rindex()_replication_with_other_methods.py
+++ batch_7/rindex()_replication_with_other_methods.py
@@ -1,5 +1,17 @@
 #initialize function for rindex() replication
+def find_rindex(user_input, find): 
+    
+    find_length = len(find)
 
-#initialize user input variable
+    for i in range(len(user_input) - find_length, -1, -1):
+        if user_input[i:i + find_length] == find:
+            return i
+    
+    raise ValueError("Not found")
+
+#initialilze user input variables
+user_input = input("Enter your desired text: ")
+find = input("Enter word you wish to find first occurence: ")
 
 #print result
+print(find_rindex(user_input, find))
