b2282af679a07f7f8a7d173a59997b160e5ddf48
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 22:10:22 +0800

    Proram 03: Finished Program, implented ord() and chr() functions to convert in uppercase letters

--- batch_7/upper()_replication_with_other_methods.py
+++ batch_7/upper()_replication_with_other_methods.py
@@ -1,5 +1,20 @@
 #initialize function for upper() replication
+#initialize list for storing result of converted letters
+def make_upper(user_input):
+    result = []
+
+    for letter in user_input:
+        if 'a' <= letter <= 'z':
+            uppercase_letter = chr(ord(letter) - (ord('a') - ord('A')))
+            result.append(uppercase_letter)
+        
+        else:
+            result.append(letter)
+    
+    return ''.join(result)
 
 #initialize user input variable
+user_input = input("Enter your desired text: ")
 
 #print result
+print(make_upper(user_input))
