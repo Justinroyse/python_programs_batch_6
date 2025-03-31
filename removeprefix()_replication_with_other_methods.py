6736134ade70e3de89612b986800d00f7da04d6e
justinroyse <justinroyselsolomon@gmail.com>
2025-03-31 15:42:41 +0800

    Program 02 Bug: Returning Nothing

--- removeprefix()_replication_with_other_methods.py
+++ removeprefix()_replication_with_other_methods.py
@@ -1,5 +1,16 @@
 #intialize function for removeprefix() replication
+def remove_prefix(removed_word):
+    remove = removed_word.replace(removed_word, "pogi")
+
+    return remove
 
 #initialize user input 
+sample_text = "JustinPogi"
+
+print(sample_text)
+
+user_input = input("Select word/s that you would like to remove: ")
 
 #print result
+
+print(remove_prefix(user_input))
